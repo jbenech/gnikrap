e55b79fc806623a366a22033a8678c4de6ff8e6a
jbenech <jbenech@free.fr>
2015-04-13 21:40:27 +0200

    Finalize import images dialog

--- gnikrap-core/src/main/WEB-CONTENT/app/main.js
+++ gnikrap-core/src/main/WEB-CONTENT/app/main.js
@@ -1120,20 +1120,39 @@ function ImportImagesViewModel(appContext) {
   var self = this;
   { // Init
     self.context = appContext; // The application context
-    self.imageAsDataURI = ko.observable("");
-
+    self.codeToBuildTheImage = ko.observable("");
+    self.useDithering = ko.observable(true);
+    self.keepAspectRatio = ko.observable(true);
+    self.threshold = ko.observable(128);
+    
     self.canvas = document.getElementById("importImagesModalCanvas"); // Canvas for displaying the image
     self.canvasCtx = self.canvas.getContext('2d');
+
+    self.currentImage = undefined;
     
     self.MAX_WIDTH = 178;
     self.MAX_HEIGHT = 128;
+    
+    self.useDithering.subscribe(function(newValue) {
+      self.__doRecomputeImage();
+    });
+    self.keepAspectRatio.subscribe(function(newValue) {
+      self.__doRecomputeImage();
+    });
+    self.threshold.subscribe(function(newValue) {
+      self.__doRecomputeImage();
+    });
   }
   
   self.display = function() {
     // Initialize the values
     $('#importImagesModal').modal('show');
+    $('#importImages_selectFileForm')[0].reset();
     
-    self.imageAsDataURI("");
+    self.currentImage = undefined;
+    self.codeToBuildTheImage("");
+    self.threshold(128);
+    self.canvasCtx.clearRect(0, 0, self.canvas.width, self.canvas.height);
   }
   
   self.uploadImage = function(file) {
@@ -1141,41 +1160,51 @@ function ImportImagesViewModel(appContext) {
     
     // Only process image files.
     if (file.type.match('image.*')) {
-      console.log(file.name  + " is an image");
       // Initialize the image object
       var newImg = new Image;
       newImg.onload = function() {
-        self.__doImageLoaded(newImg);
+        self.currentImage = { filename: file.name, rawData: newImg };
+        self.__doImageLoaded();
       }
       // Load the selected file
       var reader = new FileReader();
       reader.onload = function(event) {
-        console.log("Source of the image: " + event.target.result);
         newImg.src = event.target.result;
       };
       reader.readAsDataURL(file);
     } else {
-      console.log("Not an image !");
+      bootbox.alert(i18n.t("importImagesModal.errors.fileIsNotAnImageSelectAnother", { filename: file.name }));
+      self.currentImage = undefined;
+      $('#importImages_selectFileForm')[0].reset();
     }
   }
   
   self.hide = function() {
     $('#importImagesModal').modal('hide');
   }
-
-  self.onCopyAndClose = function() {
-    self.hide();    
+  
+  self.__doRecomputeImage = function() {
+    if(self.currentImage) {
+      self.__doImageLoaded();
+    }
+    // else: No image loaded => ignore
   }
   
-  self.__doImageLoaded = function(newImg) {
-    console.log("Image loaded ! " + newImg);
-    console.log("Image width: " + newImg.width + " / " + (self.MAX_WIDTH / newImg.width));
-    console.log("Image height: " + newImg.height + " / " + (self.MAX_HEIGHT / newImg.height));
-    var ratio = Math.min(1, (self.MAX_WIDTH / newImg.width), (self.MAX_HEIGHT / newImg.height));
-    console.log("Ratio: " + ratio);
-    var targetWidth = Math.min(self.MAX_WIDTH, Math.ceil(newImg.width * ratio));
-    var targetHeight = Math.min(self.MAX_HEIGHT, Math.ceil(newImg.height * ratio));
+  self.__doImageLoaded = function() {
+    var newImg = self.currentImage.rawData;
+    var filename = self.currentImage.filename;
+    
+    var targetWidth = Math.min(self.MAX_WIDTH, newImg.width);
+    var targetHeight = Math.min(self.MAX_HEIGHT, newImg.height);
+    
+    if(self.keepAspectRatio()) {
+      // Default: keep aspect ratio and reduce the image if needed
+      var ratio = Math.min(1, (self.MAX_WIDTH / newImg.width), (self.MAX_HEIGHT / newImg.height));
+      targetWidth = Math.min(self.MAX_WIDTH, Math.ceil(newImg.width * ratio));
+      targetHeight = Math.min(self.MAX_HEIGHT, Math.ceil(newImg.height * ratio));
+    }    
     console.log("Target width: " + targetWidth + ", target height: " + targetHeight);
+    
     self.canvasCtx.clearRect(0, 0, self.canvas.width, self.canvas.height);
     self.canvasCtx.drawImage(newImg, 0, 0, targetWidth, targetHeight);
     
@@ -1190,8 +1219,13 @@ function ImportImagesViewModel(appContext) {
         var i4 = 4*i;
         sPixels[i] = .21 * pixels[i4] + .72 * pixels[i4+1] + .07 * pixels[i4+2];
     }
-    // Dithering in white and black
-    self.__convertToWhiteAndBlackDither(targetWidth, targetHeight, sPixels);
+    if(self.useDithering()) {
+      // Dithering in white and black
+      self.__convertToWhiteAndBlackDither(targetWidth, targetHeight, self.threshold(), sPixels);
+    } else {
+      // Basic conversion
+      self.__convertToWhiteAndBlack(targetWidth, targetHeight, self.threshold(), sPixels);
+    }
     
     // Copy back pixels to context imageData
     for (var i = 0; i < numPixels; i++) {
@@ -1204,34 +1238,34 @@ function ImportImagesViewModel(appContext) {
     self.canvasCtx.putImageData(imageData, 0, 0);
     
     // Encode to data URI
-    var imageAsDataURI = self.__encodeRBGImageAsDataURI(self.__convertToRGFBinaryData(imageData.width, imageData.height, sPixels));
-    self.imageAsDataURI(imageAsDataURI);
+    var imageAsDataURI = self.__getCodeToBuildTheImage(self.__convertToRGFBinaryData(imageData.width, imageData.height, sPixels), filename);
+    self.codeToBuildTheImage(imageAsDataURI);
   }
   
   // Floyd–Steinberg dithering algorithm - See http://en.wikipedia.org/wiki/Floyd%E2%80%93Steinberg_dithering
-  self.__convertToWhiteAndBlackDither = function(width, height, pixels) {
+  // One deviation: Divide by 32 instead of 16 in order to don't report all the error (seems to give better results with only 
+  // 2 colours in the target palette)
+  self.__convertToWhiteAndBlackDither = function(width, height, threshold, pixels) {
     for(var i = 0; i < height; i++) {
       var lineOffset = i * width;
       for (var j = 0; j < width; j++) {
         var idx = lineOffset + j; // Current pixel index
         var currentPixel = pixels[idx];
-        var newPixel = (currentPixel  < 128 ? 0 : 255); // Black or white
+        var newPixel = (currentPixel  < threshold ? 0 : 255); // Black or white
         var error = currentPixel - newPixel;
         pixels[idx] = newPixel; // Set the pixel color                  
         // Report error on other pixels
-        if(Math.abs(error) > 16) { // Variation with the original algorithm: Ignore (too) small errors
-          var notLastRow = (j+1 < width);
-          if(notLastRow) {
-            pixels[idx+1] += error*7/16; // pixel at right
+        var notLastRow = (j+1 < width);
+        if(notLastRow) {
+          pixels[idx+1] += error*7/32; // pixel at right
+        }
+        if(i+1 < height) {
+          if(j > 0) {
+          pixels[idx + width - 1] += error*3/32; // Pixel at bottom left
           }
-          if(i+1 < height) {
-            if(j > 0) {
-            pixels[idx + width - 1] += error*3/16; // Pixel at bottom left
-            }
-            pixels[idx + width] += error*5/16; // Pixel at bottom
-            if(notLastRow) {
-              pixels[idx + width + 1] += error/16; // Pixel at bottom right
-            }
+          pixels[idx + width] += error*5/32; // Pixel at bottom
+          if(notLastRow) {
+            pixels[idx + width + 1] += error/32; // Pixel at bottom right
           }
         }
       }
@@ -1276,8 +1310,41 @@ function ImportImagesViewModel(appContext) {
   }
 
   // Return a Data URI representing the given RGF binary data
-  self.__encodeRBGImageAsDataURI = function(binaryData) {
-    return "data:image/rgf;base64," + btoa(String.fromCharCode.apply(null, binaryData));
+  self.__getCodeToBuildTheImage = function(binaryData, filename) {
+    var imgDataURI = "data:image/rgf;base64," + btoa(String.fromCharCode.apply(null, binaryData));
+    // Build the variable name
+    var varName;
+    try{
+      varName = filename;
+      var dotIndex = filename.lastIndexOf('.');
+      if(dotIndex != -1) {
+        varName = filename.substring(0, dotIndex);
+      }
+      varName = varName.replace(/[^a-zA-Z0-9_]/g,''); // Remove all that is not a letter or number
+      if(varName.length > 0) {
+        varName = varName.charAt(0).toUpperCase() + varName.slice(1);
+      }
+      varName = "img" + varName;
+    }catch(e){
+      console.log(e);
+      varName = "img";
+      // Just ignore, not standard filename
+    }
+    // Build the code
+    var jsCode = "var " + varName + " = ev3.getScreen().decodeImage(";
+    var index = 0, nextLineLength = Math.max(100 - jsCode.length, 10);
+    while(index < imgDataURI.length) {
+      jsCode += "\"" + imgDataURI.slice(index, index + nextLineLength) + "\"";
+      index += nextLineLength;
+      if(index < imgDataURI.length) {
+        jsCode += " + \n";
+      } else {
+        jsCode += ");";
+      }
+      nextLineLength = 100;
+    }
+    
+    return jsCode;
   }
 }
 
